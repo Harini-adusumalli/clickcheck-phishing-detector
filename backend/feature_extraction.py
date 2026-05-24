e8569f28938c55f6db36a3bc2de2e0408c4d2fe8
Harini-adusumalli <hariniadusumalli.24@gmail.com>
2026-05-24 19:08:46 +0530

    Added basic feature extraction

--- backend/feature_extraction.py
+++ backend/feature_extraction.py
@@ -1,1 +1,19 @@
+from urllib.parse import urlparse
 
+def extract_features(url):
+    # URL length
+    length = len(url)
+
+    # number of dots
+    dots = url.count(".")
+
+    # https check
+    https = 1 if url.startswith("https") else 0
+
+    return [length, dots, https]
+
+
+# test
+if __name__ == "__main__":
+    test_url = "https://google.com"
+    print(extract_features(test_url))
